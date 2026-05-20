a14f03122cc4530e0a868b4551a0e9d319bba404
marbin701 <marvin07012002@gmail.com>
2026-05-20 13:02:16 -0400

    Working on SwapBox class, missing the describe method

--- src/main/java/com/pluralsight/generics/SwapBox.java
+++ src/main/java/com/pluralsight/generics/SwapBox.java
@@ -1,4 +1,37 @@
 package com.pluralsight.generics;
 
-public class SwapBox {
+public class SwapBox<T> {
+    private T leftItem;
+    private T rightItem;
+
+    public SwapBox(T leftItem, T rightItem) {
+        this.leftItem = leftItem;
+        this.rightItem = rightItem;
+    }
+
+    public T getLeftItem() {
+        return leftItem;
+    }
+
+    public T getRightItem() {
+        return rightItem;
+    }
+
+    public void setLeftItem(T leftItem) {
+        this.leftItem = leftItem;
+    }
+
+    public void setRightItem(T rightItem) {
+        this.rightItem = rightItem;
+    }
+
+    public void swap (){
+        T tempItem = leftItem;
+        rightItem = leftItem;
+        leftItem = rightItem;
+    }
+
+    public String describe(){
+        return "";
+    }
 }
